6e76900d2ff045ecfe65b54548d0c703566f03d9
ouzhenxuan <442910407@qq.com>
2016-08-04 20:21:04 +0800

    Initial Commit

---
+++ Reconstruct/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Reconstruct
+//
+//  Created by 区振轩 on 16/8/4.
+//  Copyright © 2016年 区振轩. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Reconstruct/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Reconstruct
+//
+//  Created by 区振轩 on 16/8/4.
+//  Copyright © 2016年 区振轩. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
